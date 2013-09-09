7fcce3b6e8c651b2760a7a79ec6a588df0b8085d
ZJ <zhuojia.dai@gmail.com>
2013-09-09 16:03:39 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,23 +1,32 @@
 library(shiny)
 options(shiny.reactlog=TRUE); 
 
-renderAlert <- function(expr, env = parent.frame(), quoted = FALSE) {
-	func <- shiny::exprToFunction(expr, env, quoted)
-	function() {
-		alertText <- func()		
-		alertText
-	}
+showShinyAlert <- function(id,HTMLtext,session) {
+	session$sendCustomMessage(id,HTMLtext)
 }
 
+
+# renderAlert <- function(expr, env = parent.frame(), quoted = FALSE) {
+# 	func <- shiny::exprToFunction(expr, env, quoted)
+# 	function() {
+# 		alertText <- func()		
+# 		alertText
+# 	}
+# }
+
+
+
 # Define server logic required to generate and plot a random distribution
-shinyServer(function(input, output) {
-#  
-# 	output$actionButton_alert <- renderAlert({		
-# 		if(input$actionButton1 > 0) {
-# 			return("You pressed the button")
-# 		} else {
-# 			return("")
-# 		}
-# 	})
-#  	outputOptions(output, "actionButton_alert", suspendWhenHidden=FALSE)   
+shinyServer(function(input, output,session) {
+	no_of_showing = 0
+
+	observe({
+		if(input$actionButton1 > 0) {
+			no_of_showing <<- no_of_showing + 1
+			showShinyAlert("actionButton_alert",paste("You tried to show me",no_of_showing,"time(s)"),session)
+		} else {
+			return("")
+		}
+	})
+ 	
 })
